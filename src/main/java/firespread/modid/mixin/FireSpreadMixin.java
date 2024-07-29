8f8a55b46a6a08f52631f6c9fc2cd3d521176b5c
Kadulous <34989139+Kadulous0@users.noreply.github.com>
2024-07-28 22:50:16 -0700

    tiny changes

--- src/main/java/firespread/modid/mixin/FireSpreadMixin.java
+++ src/main/java/firespread/modid/mixin/FireSpreadMixin.java
@@ -12,17 +12,15 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FireBlock.class)
 public abstract class FireSpreadMixin {
     @Shadow protected abstract int getBurnChance(WorldView world, BlockPos pos);
-
-    @Shadow protected abstract void trySpreadingFire(World world, BlockPos pos, int spreadFactor, Random random, int currentAge);
 
     @Shadow protected abstract boolean isFlammable(BlockState state);
 
@@ -34,8 +32,7 @@ public abstract class FireSpreadMixin {
 
     @Shadow protected abstract int getSpreadChance(BlockState state);
 
-    @Shadow
-    protected static int getFireTickDelay(Random random) { return 30 + random.nextInt(10); }
+    @Shadow protected static int getFireTickDelay(Random random) { return 30 + random.nextInt(10); }
 
     @Inject(method = "getBurnChance(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)I", at = @At("RETURN"), cancellable = true)
     private void mixinBurnChance(CallbackInfoReturnable<Integer> cir) {
@@ -49,9 +46,12 @@ public abstract class FireSpreadMixin {
         cir.setReturnValue(Math.round(((float) cir.getReturnValue()) * multiplier));
     }
 
-    // REPLACE
-    @Inject(method = "scheduledTick", at = @At("HEAD"), cancellable = true)
-    private void modifyScheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+    /**
+     * @author Kadulous
+     * @reason Temporary
+     * */
+    @Overwrite
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         world.scheduleBlockTick(pos, (Block) (Object) this, getFireTickDelay(world.random));
         if (world.getGameRules().getBoolean(GameRules.DO_FIRE_TICK)) {
             if (!state.canPlaceAt(world, pos)) {
@@ -126,7 +126,6 @@ public abstract class FireSpreadMixin {
                 }
             }
         }
-        ci.cancel(); // Cancel the original method execution
     }
 
     /*/
@@ -151,9 +150,12 @@ public abstract class FireSpreadMixin {
     }
      */
 
-    // REPLACE
-    @Inject(method = "trySpreadingFire", at = @At("HEAD"), cancellable = true)
-    private void modifiedTrySpreadingFire(World world, BlockPos pos, int spreadFactor, Random random, int currentAge, CallbackInfo ci) {
+    /**
+     * @author Kadulous
+     * @reason Temporary
+     */
+    @Overwrite
+    public final void trySpreadingFire(World world, BlockPos pos, int spreadFactor, Random random, int currentAge) {
         // Custom spread logic
         if (random.nextInt(spreadFactor) < this.getSpreadChance(world.getBlockState(pos))) {
             BlockState blockState = world.getBlockState(pos);
@@ -169,6 +171,5 @@ public abstract class FireSpreadMixin {
                 TntBlock.primeTnt(world, pos);
             }
         }
-        ci.cancel(); // Cancel the original method execution
     }
 }
